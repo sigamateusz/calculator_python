a4376cf17690e5573cd20ad9e75ec3ba83b61fb8
Mateusz Siga <sigamateusz@gmail.com>
2016-10-28 17:57:29 +0200

    Now it's up to two decimal places

--- calculator.py
+++ calculator.py
@@ -8,19 +8,21 @@ def second_number():
         return n
     except ValueError:
         second_number() # up trying input is integer
-
-while 1:
-    try: # try convert input to int
-        a=int(input("Enter a number (or a letter to exit): "))
-    except ValueError:
-        exit() # if can't convert to int close the program
-    a=str(a) # convert first number to string
-    x=input("Enter an operation: ")
-    while  x!='+' and x!='-' and x!='/' and x!='*': #checks sign of input
-        x=input("It's invalid sign. Enter an operation again: ")
-    try:
-        b=int(input("Enter another number: ")) # try convert input to int
-    except ValueError:
-        b=second_number() # uses function second_number() to try again
-    b=str(b) # convert second number to string
-    print("Result : %.0f\n" %eval(a+x+b)) # eval count result 
+def main():
+    while 1:
+        try: # try convert input to int
+            a=int(input("Enter a number (or a letter to exit): "))
+        except ValueError:
+            exit() # if can't convert to int close the program
+        a=str(a) # convert first number to string
+        x=input("Enter an operation: ")
+        while  x!='+' and x!='-' and x!='/' and x!='*': #checks sign of input
+            x=input("It's invalid sign. Enter an operation again: ")
+        try:
+            b=int(input("Enter another number: ")) # try convert input to int
+        except ValueError:
+            b=second_number() # uses function second_number() to try again
+        b=str(b) # convert second number to string
+        print("Result : %.2f\n" %eval(a+x+b)) # eval count result
+if __name__=="__main__":
+    main()
